7e61b65f79dc07256072848c99b38ada939871f4
Darren Liew <ldarren@gmail.com>
2013-11-29 16:28:21 +0000

    added new error type

--- const.js
+++ const.js
@@ -27,6 +27,7 @@ Object.freeze(G_CERROR = {
     400: {code: 400, msg:'Bad Request'},
     403: {code: 403, msg:'Forbidden'},
     404: {code: 404, msg:'Not Found'},
+    419: {code: 419, msg:'Authentication Timeout'},
     500: {code: 500, msg:'Internal Server Error'},
     501: {code: 501, msg:'Not Implemented'},
 });
