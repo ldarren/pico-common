f0ffd6f0c632e5f4310c467f205d32ab278e62c8
Darren Liew <ldarren@gmail.com>
2015-06-04 07:21:20 +0000

    adapted concat from tool

--- concat.js
+++ concat.js
@@ -3,35 +3,39 @@
 var
 fs = require('fs'),
 path = require('path'),
-symPath = process.argv[1],
-dest = path.sep + (process.argv[2] || 'velvet.js'),
-srcDir = path.sep + (process.argv[3] || 'js') + path.sep
+src = process.argv[2]
+
+if (!src) return console.log('Usage: '+process.argv[1]+' pico.js,tools/obj.js [output.js]')
+
+var
+symPath = process.argv[1]
+dest = process.argv[3] || 'output.js',
+files= src.split(','),
+getPath = function(dir, file){
+    if (path.isAbsolute(file)) return file
+    return dir+path.sep+file
+}
 
 fs.readlink(symPath, function(err, realPath){
 	if (err) realPath = symPath
 	var wd = path.dirname(realPath)
-	dest = wd + dest
-	srcDir = wd + srcDir
+	dest = getPath(wd, dest)
 	console.log('delete', dest)
 	fs.unlink(dest, function(err){
-		console.log('read dir', srcDir)
-		fs.readdir(srcDir, function(err, files){
-			if (err) return console.error(err)
-			console.log('open file', dest)
-			var ws = fs.createWriteStream(dest, {flags:'a'});
-			(function(cb){
-				if (!files.length) return cb()
-				var
-				fname = files.shift(),
-				callee = arguments.callee
+        console.log('open file', dest)
+        var ws = fs.createWriteStream(dest, {flags:'a'});
+        (function(cb){
+            if (!files.length) return cb()
+            var
+            fname = files.shift(),
+            callee = arguments.callee
 
-				if ('.' === fname || '..' === fname) callee(cb)
-				console.log('appending', fname, '...')
-				var rs = fs.createReadStream(srcDir+fname)
+            if ('.' === fname || '..' === fname) callee(cb)
+            console.log('appending', fname, '...')
+            var rs = fs.createReadStream(getPath(wd,fname))
 
-				rs.on('close', function(){ callee(cb) })
-				rs.pipe(ws, {end:false})
-			})(function(){ console.log('Done!')})  
-		})      
+            rs.on('close', function(){ callee(cb) })
+            rs.pipe(ws, {end:false})
+        })(function(){ console.log('Done!')})  
 	})
 })              
