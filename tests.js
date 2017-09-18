c700e393a3368bb3caade28eb5b61f0a8a0ace03
Darren Liew <ldarren@gmail.com>
2017-09-18 10:20:04 +0000

    refactored jsonpath from obj to json

--- tests.js
+++ tests.js
@@ -1,14 +1,14 @@
 const
 globalKeys=Object.keys(global),
 pico=require('./bin/pico-cli'),
-web= pico.export('pico/web'),
-obj= pico.export('pico/obj'),
-str= pico.export('pico/str'),
-time= pico.export('pico/time'),
+pobj= pico.export('pico/obj'),
+pjson= pico.export('pico/json'),
+pstr= pico.export('pico/str'),
+ptime= pico.export('pico/time'),
 ensure= pico.export('pico/test').ensure
 
 ensure('ensure pico has loaded correctly', function(cb){
-	cb(null, obj !== undefined)
+	cb(null, pobj !== undefined)
 })
 ensure('ensure pico properties no leak', function(cb){
 	cb(null, globalKeys.length === Object.keys(global).length)
@@ -147,7 +147,7 @@ ensure('ensure obj2 override obj1. output value of key1 should be 2', function(c
 	obj1 = {key1:1},
 	obj2 = {key1:2}
 
-	cb(null, obj.extend(obj1, obj2))
+	cb(null, pobj.extend(obj1, obj2))
 })
 
 ensure('ensure obj1 merges with obj2. output should contain key1 and key2', function(cb){
@@ -155,7 +155,7 @@ ensure('ensure obj1 merges with obj2. output should contain key1 and key2', func
 	obj1 = {key1:1},
 	obj2 = {key2:2}
 
-	cb(null, obj.extend(obj1, obj2))
+	cb(null, pobj.extend(obj1, obj2))
 })
 
 ensure('compare extend to assign performance', function(cb){
@@ -166,7 +166,7 @@ ensure('compare extend to assign performance', function(cb){
     t1,t2
 
     for(var i=0; i<10000; i++){
-        obj.extend(obj1,obj2)
+        pobj.extend(obj1,obj2)
     }
     t1=Date.now()-t
 
@@ -186,7 +186,7 @@ ensure('ensure options.tidy on is working. output should not contain any undefin
 	obj1 = {key1:1},
 	obj2 = {key2:undefined}
 
-	cb(null, obj.extend(obj1, obj2, {tidy:1}))
+	cb(null, pobj.extend(obj1, obj2, {tidy:1}))
 })
 
 ensure('ensure options.tidy off is working. output should contain an undefined key', function(cb){
@@ -194,7 +194,7 @@ ensure('ensure options.tidy off is working. output should contain an undefined k
 	obj1 = {key1:1},
 	obj2 = {key2:undefined}
 
-	cb(null, obj.extend(obj1, obj2))
+	cb(null, pobj.extend(obj1, obj2))
 })
 
 ensure('ensure options.mergeArr on is working. output should contain[1,2,3] list', function(cb){
@@ -202,7 +202,7 @@ ensure('ensure options.mergeArr on is working. output should contain[1,2,3] list
 	obj1 = [1,2],
 	obj2 = [2,3]
 
-	cb(null, obj.extend(obj1, obj2, {mergeArr:1}))
+	cb(null, pobj.extend(obj1, obj2, {mergeArr:1}))
 })
 
 ensure('ensure options.mergeArr off is working. output should contain[2,3] list', function(cb){
@@ -210,23 +210,23 @@ ensure('ensure options.mergeArr off is working. output should contain[2,3] list'
 	obj1 = [1,2],
 	obj2 = [2,3]
 
-	cb(null, obj.extend(obj1, obj2))
+	cb(null, pobj.extend(obj1, obj2))
 })
 ensure('ensure function extended properly', function(cb){
 	var
 	obj1 = {func:function(){return 1}},
 	obj2 = {func:function(){return arguments.callee.prototype()}},
 	obj3 = {},
-	obj4 = obj.extend(obj3,obj1,obj2)
+	obj4 = pobj.extend(obj3,obj1,obj2)
 
 	cb(null, obj1.func()===obj4.func())
 })
 
 ensure('ensure obj.parseInts is working, ["1", "2"] should parse to [1, 2]', function(cb){
-	cb(null, obj.parseInts(['1','2']))
+	cb(null, pobj.parseInts(['1','2']))
 })
 
-ensure('ensure obj.jsonpath work', function(cb){
+ensure('ensure json.path work', function(cb){
 	var json = {
 		store: {
 			book: [
@@ -244,111 +244,111 @@ ensure('ensure obj.jsonpath work', function(cb){
 		}
 	}
 	var total = 0
-	obj.jsonpath(json)('..','price')((price)=>{total+=price; return price})()
+	pjson.path(json)('..','price')((price)=>{total+=price; return price})()
 	cb(null, 360 === total)
 })
 
 var cron='5-20/6 */9 5/5 6/3 6-0 *'
 ensure(`ensure parse cron(${cron}) correctly`, function(cb){
-	cb(null, time.parse(cron))
+	cb(null, ptime.parse(cron))
 })
 ensure('ensure get nearest cron(MIN HR DOM MON DOW YR) correctly', function(cb){
-	cb(null, (new Date(time.nearest(...time.parse(cron)))).toUTCString())
+	cb(null, (new Date(ptime.nearest(...ptime.parse(cron)))).toUTCString())
 })
 ensure('ensure weeknum of 1/Mar/2016 is 9', function(cb){
-	cb(null, 9===time.weeknum(new Date(2016,2,1,0,0,0)))
+	cb(null, 9===ptime.weeknum(new Date(2016,2,1,0,0,0)))
 })
 
 ensure('ensure left pad 8 for a number', function(cb){
-	cb(null, '00000019'===str.pad(19,8))
+	cb(null, '00000019'===pstr.pad(19,8))
 })
 ensure('ensure str.log works', function(cb){
-	str.log(null,'str.log','test')
+	pstr.log(null,'str.log','test')
 	cb(null, true)
 })
 ensure('ensure str.error works', function(cb){
-	str.error(arguments.callee,'str.error','test')
+	pstr.error(arguments.callee,'str.error','test')
 	cb(null, true)
 })
 ensure('ensure str.template works', function(cb){
 	const
-	tmpl=str.template('<%d.text%>'),
+	tmpl=pstr.template('<%d.text%>'),
 	obj={text:'Hello World'}
 
 	cb(null, obj.text===tmpl(obj))
 })
 ensure('ensure str.template mix well with js', function(cb){
-	const tmpl=str.template('<%for(var i=0; i<5; i++){%>1<%}%>')
+	const tmpl=pstr.template('<%for(var i=0; i<5; i++){%>1<%}%>')
 	cb(null, '11111'===tmpl())
 })
 ensure('ensure restful params parser supported: url/v%version/pushPackage/:pushId',function(cb){
 	var
 	route='url/v%version/pushPackage/:pushId',
-	build=str.compileRest(route),
+	build=pstr.compileRest(route),
 	params={},
-	api=str.execRest('url/v1/pushPackage/web.com.domain.app',build,params)
+	api=pstr.execRest('url/v1/pushPackage/web.com.domain.app',build,params)
 	cb(null, api===route && 1===params.version && 'web.com.domain.app'===params.pushId)
 })
 ensure('ensure restful wildcard parser supported: url/ver%version/path/#path',function(cb){
 	var
 	route='url/ver%version/path/#path',
-	build=str.compileRest(route),
+	build=pstr.compileRest(route),
 	params={},
-	api=str.execRest('url/ver1/path/web/com/domain/app',build,params)
+	api=pstr.execRest('url/ver1/path/web/com/domain/app',build,params)
 
 	cb(null, api===route && 1===params.version && 'web/com/domain/app'===params.path)
 })
 ensure('ensure restful multiple build supported: /:appName|#appPath',function(cb){
 	var
 	route='/:appName|#appPath',
-	build=str.compileRest('ERR|*msg'),
+	build=pstr.compileRest('ERR|*msg'),
 	params={}
 
-	str.compileRest(route, build)
+	pstr.compileRest(route, build)
 
-	var api=str.execRest('/msair',build,params)
+	var api=pstr.execRest('/msair',build,params)
 	cb(null, api===route && 'msair'===params.appName)
 })
 ensure('ensure restful optional parser: url/v%version|device/:deviceToken|path/#path',function(cb){
 	var
 	route='url/v%version|device/:deviceToken|path/#path',
-	build=str.compileRest(route),
+	build=pstr.compileRest(route),
 	params={},
-	api=str.execRest('url/v1/device/ab45/path/web/com/domain/app',build,params)
+	api=pstr.execRest('url/v1/device/ab45/path/web/com/domain/app',build,params)
 	if(api!==route || 1!==params.version || 'ab45'!==params.deviceToken || 'web/com/domain/app'!==params.path) return cb(null, false)
 	params={}
-	api=str.execRest('url/v1/device/ab45',build,params)
+	api=pstr.execRest('url/v1/device/ab45',build,params)
 	if(api!==route || 1!==params.version || 'ab45'!==params.deviceToken) return cb(null, false)
 	params={}
-	api=str.execRest('url/v1',build,params)
+	api=pstr.execRest('url/v1',build,params)
 	if(api!==route || 1!==params.version) return cb(null, false)
 	cb(null, true)
 })
 ensure('ensure restful optional parser2: /:appName|#appPath',function(cb){
 	var
 	route='/:appName|#appPath',
-	build=str.compileRest(route),
+	build=pstr.compileRest(route),
 	params={},
-	api=str.execRest('/msair',build,params)
+	api=pstr.execRest('/msair',build,params)
 	cb(null, api===route && 'msair'===params.appName)
 })
 ensure('ensure codec encode string "{"data":123}" and decode to the same', function(cb){
 	var
 	data = JSON.stringify({data:123}),
 	key = parseInt('100007900715391')
-	cb(null, data===str.codec(key, str.codec(key, data)))
+	cb(null, data===pstr.codec(key, pstr.codec(key, data)))
 })
 ensure('ensure codec work on time based string', (cb)=>{
 	const
 	key='00mjvyn50022oq0000zbpt6c000014k2',
 	secret='3zuklpkl6k905e5kryoiozuxrkjhunr26vjnlaao',
 	now=Math.floor(Date.now()/(5*60*1000)),
-	hash=now+str.hash(secret),
-	token=str.codec(hash,key)
-	cb(null, key===str.codec(key, str.codec(hash, token)))
+	hash=now+pstr.hash(secret),
+	token=pstr.codec(hash,key)
+	cb(null, key===pstr.codec(key, pstr.codec(hash, token)))
 })
 ensure('ensure hash password to 32bit int', function(cb){
-	cb(null, str.hash('免费服务会立即翻译英文和英文之间的单词'))
+	cb(null, pstr.hash('免费服务会立即翻译英文和英文之间的单词'))
 })
 ensure('ensure hash dont collide in repeating char x9999', function(cb){
 	var
@@ -357,7 +357,7 @@ ensure('ensure hash dont collide in repeating char x9999', function(cb){
 	l=9999,
 	n=Date.now()
 	for(var i=0,h; i<l; i++){
-		h=str.hash(s)
+		h=pstr.hash(s)
 		if (~hist.indexOf(h)) break
 		hist.push(h)
 		s+='p'
@@ -371,7 +371,7 @@ ensure('ensure hash dont collide in uuid x99999', function(cb){
 	l=99999,
 	n=Date.now()
 	for(var i=0,h; i<l; i++){
-		h=str.hash(str.rand())
+		h=pstr.hash(pstr.rand())
 		if (~hist.indexOf(h)) break
 		hist.push(h)
 	}
